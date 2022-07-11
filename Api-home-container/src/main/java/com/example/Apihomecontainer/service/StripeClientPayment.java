8b8b92aaba26420ebfe8c2c118badea59469dba4
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-07-11 17:58:16 +0300

    Remove unnecessary functions stripe class

--- Api-home-container/src/main/java/com/example/Apihomecontainer/service/StripeClientPayment.java
+++ Api-home-container/src/main/java/com/example/Apihomecontainer/service/StripeClientPayment.java
@@ -20,18 +20,6 @@ public class StripeClientPayment {
         Stripe.apiKey = secretKeyStripe;
     }
 
-    public Customer createCustomer(String token
-            , String email) throws Exception {
-        Map<String, Object> customerParams = new HashMap<>();
-        customerParams.put("email", email);
-        customerParams.put("source", token);
-        return Customer.create(customerParams);
-    }
-
-    private Customer getCustomer(String id) throws Exception {
-        return Customer.retrieve(id);
-    }
-
     public Charge chargeNewCard(String token, double amount) throws Exception {
         Map<String, Object> chargeParams = new HashMap<>();
         chargeParams.put("amount", (int) (amount * 100));
@@ -40,13 +28,4 @@ public class StripeClientPayment {
         return Charge.create(chargeParams);
     }
 
-    public Charge chargeCustomerCard(String customerId, int amount) throws Exception {
-        String sourceCard = getCustomer(customerId).getDefaultSource();
-        Map<String, Object> chargeParams = new HashMap<>();
-        chargeParams.put("amount", amount);
-        chargeParams.put("currency", "USD");
-        chargeParams.put("customer", customerId);
-        chargeParams.put("source", sourceCard);
-        return Charge.create(chargeParams);
-    }
 }
